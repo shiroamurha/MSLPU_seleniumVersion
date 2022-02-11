9be5d446cf582679732b9c0c8379ec272b8391d1
Shiro Amurha <46139101+shiroamurha@users.noreply.github.com>
2022-02-11 19:22:08 -0300

    driver update

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from selenium import webdriver
+import os
+from time import sleep
 
 
-driver = webdriver.Opera()
+local_path = rf'{os.path.dirname(os.path.realpath(__file__))}\\chromedriver.exe'
+
+driver = webdriver.Chrome(executable_path=local_path)
+
+driver.get('https://www.google.com')
+sleep(3)
+driver.close()
